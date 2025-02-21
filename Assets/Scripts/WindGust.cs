23c401675fd72ff7b550451482cd3c87efe1f27b
amezzles <amy.l.roche@student.uts.edu.au>
2025-02-21 12:51:16 +1100

    initial code

--- Assets/Scripts/WindGust.cs
+++ Assets/Scripts/WindGust.cs
@@ -10,17 +10,17 @@ public class WindGust : MonoBehaviour
     void Start()
     {
         startingLocation = new Vector2(Random.Range(-8f, 8f), Random.Range(-4f, 4f));
-        windDirection = new Vector2(Random.Range(-1f, 1f11, Random.Range(-1f, 1f)).normalized;
+        windDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         transform.position = startingLocation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(windDirection * windSpeed * Time.deltaTime;
+        transform.Translate(windDirection * windSpeed * Time.deltaTime);
     }
 
-    public WindGust CreateWindGust()
+    public static WindGust CreateWindGust(GameObject windGustPrefab, Vector2 position, Quaternion rotation)
     {
         // Create and spawn wind gust object (to be used in GameManager)
         GameObject windGustObject = Instantiate(windGustPrefab, position, rotation);
